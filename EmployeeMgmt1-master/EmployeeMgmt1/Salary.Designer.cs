6090999bc4ae517200656b34f297a8af0245f0b3
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:11:16 +0200

    label5.ForeColor = System.Drawing.Color.Teal

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -116,7 +116,7 @@ namespace EmployeeMgmt1
             // 
             this.label5.AutoSize = true; 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Teal;
+            this.label5.ForeColor = System.Drawing.Color.Teal; 
             this.label5.Location = new System.Drawing.Point(47, 300);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(74, 25);
