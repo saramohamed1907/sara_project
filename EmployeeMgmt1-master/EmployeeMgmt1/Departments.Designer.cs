3283d1a84fce54cdcbf06f8dd7c58c859d3c6552
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:31:34 +0200

    .pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")))

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -244,7 +244,7 @@
             // pictureBox1 
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Teal; 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image"))); 
             this.pictureBox1.Location = new System.Drawing.Point(105, 116);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(61, 45);
