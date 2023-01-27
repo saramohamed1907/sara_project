a06cd93306de7502b1a28e0b5104e8ea7b9ec0a2
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:00:05 +0200

    private System.ComponentModel.IContainer components = null;

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -7,7 +7,7 @@ namespace EmployeeMgmt1
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private System.ComponentModel.IContainer components = null; 
 
         /// <summary>
         /// Clean up any resources being used.
