5d523c64d369049226568226168953ae58536ed2
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:11:54 +0200

    AddBtn.Text = "Add

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -214,7 +214,7 @@
             this.AddBtn.Name = "AddBtn"; 
             this.AddBtn.Size = new System.Drawing.Size(117, 35); 
             this.AddBtn.TabIndex = 72; 
-            this.AddBtn.Text = "Add";
+            this.AddBtn.Text = "Add"; 
             this.AddBtn.UseVisualStyleBackColor = false;
             this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
             // 
