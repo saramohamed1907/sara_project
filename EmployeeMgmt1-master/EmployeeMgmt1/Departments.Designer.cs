4f9bca91474a9b7150aad5b64d8b6cca411f6097
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 21:47:08 +0200

    SalaryLbl.TabIndex = 79;

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -145,7 +145,7 @@
             this.SalaryLbl.Location = new System.Drawing.Point(722, 125); 
             this.SalaryLbl.Name = "SalaryLbl"; 
             this.SalaryLbl.Size = new System.Drawing.Size(74, 25); 
-            this.SalaryLbl.TabIndex = 79;
+            this.SalaryLbl.TabIndex = 79; 
             this.SalaryLbl.Text = "Salary";
             this.SalaryLbl.Click += new System.EventHandler(this.label10_Click);
             // 
