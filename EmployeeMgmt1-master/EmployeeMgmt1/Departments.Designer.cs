7d0859fbce481303fb73a154c40e90610dcfb988
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:40:24 +0200

    DepNameTb.Name = "DepNameTb"

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -265,7 +265,7 @@
             // 
             this.DepNameTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DepNameTb.Location = new System.Drawing.Point(32, 206); 
-            this.DepNameTb.Name = "DepNameTb";
+            this.DepNameTb.Name = "DepNameTb"; 
             this.DepNameTb.Size = new System.Drawing.Size(304, 30);
             this.DepNameTb.TabIndex = 59;
             // 
