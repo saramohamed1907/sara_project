ceb52d01fded470f9ba5a6c6ca2ecc316041b22a
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:07:24 +0200

    .pictureBox1.BackColor = System.Drawing.Color.Teal

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -94,7 +94,7 @@ namespace EmployeeMgmt1
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Teal;
+            this.pictureBox1.BackColor = System.Drawing.Color.Teal; 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(218, 125);
             this.pictureBox1.Name = "pictureBox1";
