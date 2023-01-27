ab95d25e4fc83fb4d862b3d117d1d7a0f1b7e7af
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:47:51 +0200

    label6.ForeColor = System.Drawing.Color.Teal

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -273,7 +273,7 @@
             // 
             this.label6.AutoSize = true; 
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Teal;
+            this.label6.ForeColor = System.Drawing.Color.Teal; 
             this.label6.Location = new System.Drawing.Point(27, 178);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(182, 25);
