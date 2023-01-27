01da695df4e02d9da18edcc27d2ce6c0ccf56507
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:49:47 +0200

    label2.Location = new System.Drawing.Point(446, 169)

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -285,7 +285,7 @@
             this.label2.AutoSize = true; 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Teal; 
-            this.label2.Location = new System.Drawing.Point(446, 169);
+            this.label2.Location = new System.Drawing.Point(446, 169); 
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(206, 25);
             this.label2.TabIndex = 57;
