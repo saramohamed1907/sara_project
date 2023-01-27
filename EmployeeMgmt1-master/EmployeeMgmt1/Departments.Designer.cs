3054f5b023f95a256c58a0e9fffaaffacdf85c6c
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:14:00 +0200

    label1.ForeColor = System.Drawing.Color.White

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -221,7 +221,7 @@
             // label1 
             // 
             this.label1.AutoSize = true; 
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.White; 
             this.label1.Location = new System.Drawing.Point(499, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(369, 25);
