63a5d13da5d14f2be375387fd7b6b4556a74b065
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:01:52 +0200

    UpdateBtn.ForeColor = System.Drawing.Color.Black

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -71,7 +71,7 @@ namespace EmployeeMgmt1
             //  
             this.UpdateBtn.BackColor = System.Drawing.Color.Turquoise; 
             this.UpdateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UpdateBtn.ForeColor = System.Drawing.Color.Black;
+            this.UpdateBtn.ForeColor = System.Drawing.Color.Black; 
             this.UpdateBtn.Location = new System.Drawing.Point(239, 547);
             this.UpdateBtn.Name = "UpdateBtn";
             this.UpdateBtn.Size = new System.Drawing.Size(117, 35);
