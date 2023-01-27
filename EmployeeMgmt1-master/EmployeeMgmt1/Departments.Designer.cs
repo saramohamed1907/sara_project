7c11a0a8f52b26bbbdc6101d3dbe3fe56142a376
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:24:51 +0200

    label1.Size = new System.Drawing.Size(369, 25)

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -222,9 +222,9 @@
             // 
             this.label1.AutoSize = true; 
             this.label1.ForeColor = System.Drawing.Color.White; 
-            this.label1.Location = new System.Drawing.Point(499, 9);
+            this.label1.Location = new System.Drawing.Point(499, 9); 
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(369, 25);
+            this.label1.Size = new System.Drawing.Size(369, 25); 
             this.label1.TabIndex = 0;
             this.label1.Text = "Employee mangement system version 10";
             // 
