c75c712cfa84330d40d95a684a78c271447a393f
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 21:50:05 +0200

    pictureBox4.Size = new System.Drawing.Size(42, 28);

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -155,7 +155,7 @@
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image"))); 
             this.pictureBox4.Location = new System.Drawing.Point(451, 122); 
             this.pictureBox4.Name = "pictureBox4"; 
-            this.pictureBox4.Size = new System.Drawing.Size(42, 28);
+            this.pictureBox4.Size = new System.Drawing.Size(42, 28); 
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 78;
             this.pictureBox4.TabStop = false;
