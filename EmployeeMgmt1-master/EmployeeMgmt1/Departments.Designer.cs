f694cc5011a129a95100e9eafaffb5ea75b4e9a3
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:49:20 +0200

    label2.AutoSize = true

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -282,7 +282,7 @@
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
+            this.label2.AutoSize = true; 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Teal;
             this.label2.Location = new System.Drawing.Point(446, 169);
