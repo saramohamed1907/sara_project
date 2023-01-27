04b304238edfc49d8b8177b2104a5fd3357d1e78
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:53:13 +0200

    label3.Name = "label3"

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -307,7 +307,7 @@
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Teal; 
             this.label3.Location = new System.Drawing.Point(553, 206); 
-            this.label3.Name = "label3";
+            this.label3.Name = "label3"; 
             this.label3.Size = new System.Drawing.Size(148, 25);
             this.label3.TabIndex = 83;
             this.label3.Text = "Department List";
