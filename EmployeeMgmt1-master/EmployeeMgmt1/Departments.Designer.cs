a715f35660d09af797f68e29b2d0defcd2ec61fa
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:50:42 +0200

    label2.Text = "Mange Departments"

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -289,7 +289,7 @@
             this.label2.Name = "label2"; 
             this.label2.Size = new System.Drawing.Size(206, 25); 
             this.label2.TabIndex = 57; 
-            this.label2.Text = "Mange Departments";
+            this.label2.Text = "Mange Departments"; 
             // 
             // panel1
             // 
