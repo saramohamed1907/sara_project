81602010e7d22f24adcdb4536a36473974fef047
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:55:25 +0200

    DeleteBtn.Text = "Delete"

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -319,7 +319,7 @@
             this.DeleteBtn.Name = "DeleteBtn"; 
             this.DeleteBtn.Size = new System.Drawing.Size(102, 38); 
             this.DeleteBtn.TabIndex = 84; 
-            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.Text = "Delete"; 
             this.DeleteBtn.UseVisualStyleBackColor = false;
             this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
             // 
