193ecf1a4242f2e03685b9bed7ab39b42e67d243
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 22:02:29 +0200

    .EmpLbl.Size = new System.Drawing.Size(107, 25)

--- EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Departments.Designer.cs
@@ -189,7 +189,7 @@
             this.EmpLbl.ForeColor = System.Drawing.Color.Teal; 
             this.EmpLbl.Location = new System.Drawing.Point(299, 125); 
             this.EmpLbl.Name = "EmpLbl"; 
-            this.EmpLbl.Size = new System.Drawing.Size(107, 25);
+            this.EmpLbl.Size = new System.Drawing.Size(107, 25); 
             this.EmpLbl.TabIndex = 75;
             this.EmpLbl.Text = "Employee";
             this.EmpLbl.Click += new System.EventHandler(this.EmpLbl_Click);
