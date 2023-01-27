91a70d96cebe9bf151b36d389617ddd475da212f
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:00:32 +0200

    if (disposing && (components != null))

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -15,7 +15,7 @@ namespace EmployeeMgmt1
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing) 
         {
-            if (disposing && (components != null))
+            if (disposing && (components != null)) 
             {
                 components.Dispose();
             }
