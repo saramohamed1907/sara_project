d2baa259c169bbc1842524f1f37a44d247a01fee
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:00:18 +0200

    protected override void Dispose(bool disposing)

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -13,7 +13,7 @@ namespace EmployeeMgmt1
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        protected override void Dispose(bool disposing) 
         {
             if (disposing && (components != null))
             {
