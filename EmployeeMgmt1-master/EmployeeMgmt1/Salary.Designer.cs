afde326236c9f71219709f320511993390fcdd65
saramohamed1907 <sara1907mohamed@gmail.com>
2023-01-27 23:11:03 +0200

    label5.AutoSize = true

--- EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
+++ EmployeeMgmt1-master/EmployeeMgmt1/Salary.Designer.cs
@@ -114,7 +114,7 @@ namespace EmployeeMgmt1
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
+            this.label5.AutoSize = true; 
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Teal;
             this.label5.Location = new System.Drawing.Point(47, 300);
